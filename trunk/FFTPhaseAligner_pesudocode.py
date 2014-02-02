70ddff221a398faab58e89770dc2992bd8016844
junh1024@gmail.com <junh1024@gmail.com@11690af8-6cf3-825b-adce-e340965e510a>
2014-02-02 09:58:32 +0000

    update pseudocode

--- trunk/FFTPhaseAligner_pesudocode.py
+++ trunk/FFTPhaseAligner_pesudocode.py
@@ -5,23 +5,24 @@ use $pi?
 fftsize=4096
 
 
-fft of 1&2, 3&4
+fft of 1&2, 3&4, stored in fft1-4
 
 get magnitude of in1&2, store in array
 
 #compute mags of 12
+
+#it seems that most FFT JSFX plugins in R combine ch1&2 into one blob to be fft'd (as an optimization?)
+#it may be fine for their purposes, but since i'm shifting phase, that may not be a good idea. Also, i try to keep FFTs of different channels separate for clarity.
+
 for(i in fftsize):
 	Magnitudes12[i]=sqrt(re^2+im^2)
-	
-	
-	Magnitudes12[i]=sqrt(fft1[i*2]^2+fft1[i*2+1])#JS ver, FFT in JSFX is even=Re , odd=Im?
+
+	Magnitudes1[i]=sqrt(fft1[i*2]^2+fft1[i*2+1])#JS ver, FFT in JSFX is even=Re , odd=Im?
+	Magnitudes2[i]=sqrt(fft2[i*2]^2+fft2[i*2+1])#JS ver, FFT in JSFX is even=Re , odd=Im?
+	Magnitudes3[i]=sqrt(fft3[i*2]^2+fft3[i*2+1])#JS ver, FFT in JSFX is even=Re , odd=Im?
+	Magnitudes4[i]=sqrt(fft4[i*2]^2+fft4[i*2+1])#JS ver, FFT in JSFX is even=Re , odd=Im?
 
 get angle of in3&4, store in Angles34
-
-
-#compute mags of 34
-for(i in fftsize):
-	Magnitudes34[i]=sqrt(re^2+im^2)
 
 #compute angles of 34
 for (i in fftsize):
